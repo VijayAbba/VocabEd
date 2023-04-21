64975b3284109a39ba87eaa5f0bab3d6b1f5693c
VijayAbba <vijaykrishna.ed@gmail.com>
2023-04-21 11:55:46 +0530

    deleted mapp.js

--- src/App.js
+++ src/App.js
@@ -17,42 +17,52 @@ export default function App() {
     {
       web: `https://www.google.com/search?igu=1&ei=&q=${cloze} meaning`,
       id: 1,
+      title: "google",
     },
     {
       web: `https://dictionary.cambridge.org/dictionary/english/${cloze}`,
       id: 2,
+      title: "cambridge",
     },
     {
       web: `https://www.dictionary.com/browse/${cloze}`,
       id: 3,
+      title: "dictionary",
     },
     {
       web: `https://www.vocabulary.com/dictionary/${cloze}`,
       id: 4,
+      title: "vocabulary",
     },
     {
       web: `https://www.urbandictionary.com/define.php?term=${cloze}`,
       id: 5,
+      title: "urbandictionary",
     },
     {
       web: `https://en.wiktionary.org/wiki/${cloze}`,
       id: 6,
+      title: "wiktionary",
     },
     {
       web: `https://www.thefreedictionary.com/${cloze}`,
       id: 7,
+      title: "thefreedictionary",
     },
     {
       web: `https://www.wordhippo.com/what-is/another-word-for/${cloze}.html`,
       id: 8,
+      title: "wordhippo",
     },
     {
       web: `https://visuwords.com/${cloze}`,
       id: 9,
+      title: "visuwords",
     },
     {
       web: `https://www.pexels.com/search/${cloze}`,
       id: 10,
+      title: "pexels",
     },
   ];
 
@@ -79,7 +89,12 @@ export default function App() {
         {websites.map((eachWeb) => (
           <iframe
             key={eachWeb.id}
-            sandbox="allow-scripts"
+            credentialless="true"
+            sandbox={
+              eachWeb.title === "google" ? "allow-scripts" : "allow-same-origin"
+            }
+            scrolling="yes"
+            title={eachWeb.title}
             className="h-[580px] w-[95vw] md:w-[340px] md:m-3 drop-shadow-2xl text-xs rounded-md border-8 md:border-4 border-amber-300"
             src={eachWeb.web}
           ></iframe>
